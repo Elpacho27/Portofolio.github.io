445aad1090b5d340f5ac2cbee029b70784d3cc37
Elpacho27 <patrik27ocelic03@gmail.com>
2024-12-17 21:31:58 +0100

    Fix

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 
-window.addEventListener("scroll", function() {
-    const header = document.querySelector("header");
-    if (window.scrollY > 50) {
-        header.classList.add("scrolled");
+let lastScrollTop = 0; 
+const header = document.querySelector('header'); 
+
+window.addEventListener('scroll', () => {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+        
+        header.classList.add('hide');
     } else {
-        header.classList.remove("scrolled");
+       
+        header.classList.remove('hide');
     }
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; 
 });
 
 document.addEventListener("scroll", function() {
@@ -16,4 +24,29 @@ document.addEventListener("scroll", function() {
             card.classList.add("visible");
         }
     });
+});
+
+const slider = document.querySelector('.projects-slider');
+const prevButton = document.querySelector('.prev-btn');
+const nextButton = document.querySelector('.next-btn');
+
+let slideIndex = 0; 
+
+const totalSlides = document.querySelectorAll('.project').length;
+
+
+function moveSlider() {
+    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
+
+
+nextButton.addEventListener('click', () => {
+    slideIndex = (slideIndex + 1) % totalSlides; 
+    moveSlider();
+});
+
+
+prevButton.addEventListener('click', () => {
+    slideIndex = (slideIndex - 1 + totalSlides) % totalSlides; 
+    moveSlider();
 });
